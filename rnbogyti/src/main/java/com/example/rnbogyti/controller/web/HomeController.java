9b801656c984637cc66dd8b12ee022d224ea67f7
Kefka-esque <linkbabineau@gmail.com>
2025-06-17 21:21:40 -0400

    -Added a user select page as the landing page which redirects to the old home page
    -Changed the home page a bit, removed the redundant confirm for 6pm button.
    
    -Added a stylesheet for some mobile-friendly styling and buttons + so it doesn't look like garbage
    
    -Added yet more buttons to html pages where they were needed.

--- rnbogyti/src/main/java/com/example/rnbogyti/controller/web/HomeController.java
+++ rnbogyti/src/main/java/com/example/rnbogyti/controller/web/HomeController.java
@@ -6,16 +6,20 @@ import com.example.rnbogyti.entity.UserExercise;
 import com.example.rnbogyti.service.ExerciseService;
 import com.example.rnbogyti.service.UserExerciseService;
 import com.example.rnbogyti.service.UserService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.time.LocalDate;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class HomeController {
@@ -30,7 +34,17 @@ public class HomeController {
     private UserExerciseService userExerciseService;
     
     @GetMapping("/")
-    public String home(Model model) {
+    public String selectUser(Model model){
+        List<User> users = userService.getAllUsers();
+        model.addAttribute("users", users);
+        return "select-user";
+    }
+
+    // The old home page, now per-user so the time functions can be nicer. Whole thing is still WIP though.
+    // Some pages that should point to user hoem still point to the "/" home; but this should be fixed in the future 
+    // once I introduce http sessions; there will be a redirect to user home if a user id is already in the session 
+    @GetMapping("/user/home/{id}")
+    public String home(@PathVariable Long id, Model model) {
         // 1. Get list of users and exercises
         List<User> users = userService.getAllUsers();
         List<Exercise> exercises = exerciseService.getAllExercises();
@@ -51,8 +65,12 @@ public class HomeController {
             }
             exerciseUserWeights.put(exercise.getName(), userWeights);
         }
+
+        User user = userService.getUserById(id);
+
         // Attributes the page uses
         model.addAttribute("users", users);
+        model.addAttribute("user", user);
         model.addAttribute("exerciseUserWeights", exerciseUserWeights);
         model.addAttribute("today", LocalDate.now());
         model.addAttribute("message", "Welcome to the Rainbow Boys Gym Time app!");
